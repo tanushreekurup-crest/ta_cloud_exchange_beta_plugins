ba1f7b2ca69011852256682023d5be04b07d81c1
Chirag Wadhwani <chirag.wadhwani@CDSYS.LOCAL>
2022-11-28 18:00:59 +0530

    ARE BitSight 1.1.0 - Changed plugin name from ThirdPartyTrust Application Risk Exchange to BitSight

--- third_party_trust_grc/main.py
+++ third_party_trust_grc/main.py
@@ -1,28 +1,61 @@
-"""_summary_"""
-from datetime import datetime
-import json
-import requests
-import time
+"""
+BSD 3-Clause License
 
-from netskope.integrations.grc.plugin_base import (
-    PluginBase,
-    ValidationResult,
-    PushResult,
-)
+Copyright (c) 2021, Netskope OSS
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+"""BitSight ARE Plugin."""
+
+import json
+import time
+from datetime import datetime
+
+import requests
 from netskope.common.utils import add_user_agent
 from netskope.integrations.grc.models.configuration import (
-    TargetMappingFields,
     MappingType,
+    TargetMappingFields,
+)
+from netskope.integrations.grc.plugin_base import (
+    PluginBase,
+    PushResult,
+    ValidationResult,
 )
 
 MAX_RETRY_COUNT = 4
+PLUGIN_NAME = "BitSight ARE Plugin"
 
 
-class ThirdPartyTrust(PluginBase):
-    """PLugin implementation for ThirdPartyTrust"""
+class BitSightARE(PluginBase):
+    """BitSightARE plugin Class."""
 
     def api_call_helper(self, url, method, data=None):
-        """This method help to collect data from ThirdPartyTrust."""
+        """This method help to collect data from BitSight."""
         config = self.configuration
         request_func = getattr(requests, method)
         headers = {"Authorization": f"Token {config['api_key']}"}
@@ -32,7 +65,11 @@ class ThirdPartyTrust(PluginBase):
         response = {}
         for attempt in range(MAX_RETRY_COUNT):
             resp = request_func(
-                url=url, headers=headers, verify=verify, proxies=proxies, data=data
+                url=url,
+                headers=headers,
+                verify=verify,
+                proxies=proxies,
+                data=data,
             )
             response_json = self.handle_response(resp)
             if resp.status_code == 200 or resp.status_code == 201:
@@ -40,7 +77,9 @@ class ThirdPartyTrust(PluginBase):
                 break
             elif resp.status_code == 429 and attempt < (MAX_RETRY_COUNT - 1):
                 self.logger.info(
-                    f"Too many requests occurred for {url}, retrying to make the API call. Retry count: {attempt + 1}."
+                    f"{PLUGIN_NAME}: Too many requests occurred for {url}, "
+                    "retrying to make the API call. "
+                    f"Retry count: {attempt + 1}."
                 )
                 time.sleep(60)
         else:
@@ -48,7 +87,7 @@ class ThirdPartyTrust(PluginBase):
         return response
 
     def pull_vendors(self):
-        """To pull data from ThirdPartyTrust."""
+        """To pull data from BitSight."""
         try:
             config = self.configuration
             url = f"{config['url'].strip('/')}/api/v2/connections.inactives"
@@ -62,9 +101,13 @@ class ThirdPartyTrust(PluginBase):
                 "Unable to establish connection, server is not reachable."
             )
         except requests.exceptions.HTTPError as ex:
-            raise requests.HTTPError(f"Error occurred while pulling data. {ex}")
+            raise requests.HTTPError(
+                f"Error occurred while pulling data. {ex}"
+            )
         except Exception as ex:
-            raise requests.HTTPError(f"Error occurred while pulling data. Error: {ex}")
+            raise requests.HTTPError(
+                f"Error occurred while pulling data. Error: {ex}"
+            )
 
     def add_query_list(self, final_list, mapping_dict):
         """To create final dict for mapping to the destination."""
@@ -91,19 +134,29 @@ class ThirdPartyTrust(PluginBase):
         query_builder_dict["query_builder_list"] = final_list
         return query_builder_dict
 
-    def check_result(self, tpt_record_value, application_value):
+    def check_result(self, bitsight_record_value, application_value):
         """To check value of the source fields is matching with the value of application field or not."""
-        tpt_record_value = tpt_record_value.lower() if isinstance(tpt_record_value, str) else tpt_record_value
+        bitsight_record_value = (
+            bitsight_record_value.lower()
+            if isinstance(bitsight_record_value, str)
+            else bitsight_record_value
+        )
         if isinstance(application_value, list):
             for app_val in application_value:
-                app_val = app_val.lower() if isinstance(app_val, str) else app_val
-                if app_val == tpt_record_value:
+                app_val = (
+                    app_val.lower() if isinstance(app_val, str) else app_val
+                )
+                if app_val == bitsight_record_value:
                     return True
             else:
                 return False
 
-        application_value = application_value.lower() if isinstance(application_value, str) else application_value
-        return tpt_record_value == application_value
+        application_value = (
+            application_value.lower()
+            if isinstance(application_value, str)
+            else application_value
+        )
+        return bitsight_record_value == application_value
 
     def list_of_application(self, final_dict, uuid, app):
         """Final dict created with uuid of each vendor."""
@@ -114,20 +167,19 @@ class ThirdPartyTrust(PluginBase):
         return final_dict
 
     def push(self, applications, mapping):
-        """push method to store the data collected from the Netskope tenant into ThirdPartyTrust VRM.
-        Args:
-            applications (_type_): _description_
-        """
-        self.logger.info("Plugin ThirdPartyTrust: Executing push method.")
+        """push method to store the data collected from the Netskope tenant into BitSight VRM."""
+        self.logger.info(f"{PLUGIN_NAME}: Executing push method.")
         config = self.configuration
         query_builder_dict = self.get_query_builder_list(mapping)
         vendor_results = self.pull_vendors()
         if not vendor_results:
             self.logger.info(
-                "No monitored vendors found on ThirdPartyThrust. Hence skipping the sharing."
+                f"{PLUGIN_NAME}: No monitored vendors found on \
+                    BitSight. Hence skipping the sharing."
             )
             return PushResult(
-                success=True, message="Successfully pushed data to ThirdPartyTrust."
+                success=True,
+                message="Successfully pushed data to BitSight.",
             )
         final_dict = {}
         operation = query_builder_dict.get("operation")
@@ -142,9 +194,13 @@ class ThirdPartyTrust(PluginBase):
                     continue
                 for inner_list in query_builder_dict["query_builder_list"]:
                     count = count + 1
-                    tpt_records = vendor.get("company", {}).get(f"{inner_list['lhs']}")
+                    bitsight_records = vendor.get("company", {}).get(
+                        f"{inner_list['lhs']}"
+                    )
                     applications_value = app_dict.get(inner_list["rhs"])
-                    res = self.check_result(tpt_records, applications_value)
+                    res = self.check_result(
+                        bitsight_records, applications_value
+                    )
                     if res:
                         if operation == "or" or count == len(
                             query_builder_dict["query_builder_list"]
@@ -159,7 +215,10 @@ class ThirdPartyTrust(PluginBase):
             if not check_match:
                 skip_count += 1
                 self.logger.warn(
-                    f"Application '{app_dict.get('applicationName')}' match is not available in ThirdPartyTrust, skipping sharing of this application."
+                    f"{PLUGIN_NAME}: Application "
+                    f"'{app_dict.get('applicationName')}' match is not "
+                    "available in BitSight, skipping sharing of "
+                    "this application."
                 )
         current_time = datetime.now()
         current_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
@@ -184,17 +243,21 @@ class ThirdPartyTrust(PluginBase):
                     "Unable to establish connection, server is not reachable."
                 )
             except requests.exceptions.HTTPError as ex:
-                raise requests.HTTPError(f"Error occurred while pushing data. {ex}")
+                raise requests.HTTPError(
+                    f"Error occurred while pushing data. {ex}"
+                )
             except Exception as ex:
                 raise requests.HTTPError(
                     f"Error occurred while pushing data. Error: {ex}"
                 )
         self.logger.info(
-            f"Total {shared_count} applications shared successfully with ThirdPartyTrust and "
-            f"{skip_count} applications were skipped."
+            f"{PLUGIN_NAME}: "
+            f"Total {shared_count} applications shared successfully with "
+            f"BitSight and {skip_count} applications were skipped."
         )
         return PushResult(
-            success=True, message="Successfully pushed data to ThirdPartyTrust."
+            success=True,
+            message="Successfully pushed data to BitSight.",
         )
 
     def validate(self, data):
@@ -209,20 +272,26 @@ class ThirdPartyTrust(PluginBase):
             cte.plugin_base.ValidateResult: ValidateResult object with success flag and message.
         """
         self.logger.info(
-            "ThirdPartyTrust Plugin: Executing validate method for ThirdPartyTrust plugin."
+            f"{PLUGIN_NAME}: Executing validate method for BitSight plugin."
         )
         if "url" not in data or not data["url"] or type(data["url"]) != str:
             self.logger.error(
-                "ThirdPartyTrust Plugin: Validation error occured Error: Invalid ThirdPartyTrust instance URL provided."
+                f"{PLUGIN_NAME}: Validation error occured Error:"
+                " Invalid BitSight instance URL provided."
             )
             return ValidationResult(
                 success=False,
-                message="Invalid ThirdPartyTrust Instance URL provided.",
+                message="Invalid BitSight Instance URL provided.",
             )
 
-        if "api_key" not in data or not data["api_key"] or type(data["api_key"]) != str:
+        if (
+            "api_key" not in data
+            or not data["api_key"]
+            or type(data["api_key"]) != str
+        ):
             self.logger.error(
-                "ThirdPartyTrust Plugin: Validation error occured Error: Invalid api_key provided."
+                f"{PLUGIN_NAME}: Validation error occured Error:"
+                " Invalid api_key provided."
             )
             return ValidationResult(
                 success=False,
@@ -239,7 +308,8 @@ class ThirdPartyTrust(PluginBase):
             )
             if response.status_code in [401, 403]:
                 self.logger.error(
-                    f"ThirdPartyTrust Plugin: HTTP request returned with status code {response.status_code}."
+                    f"{PLUGIN_NAME}: HTTP request returned with status "
+                    f"code {response.status_code}."
                 )
                 return ValidationResult(
                     success=False,
@@ -247,7 +317,8 @@ class ThirdPartyTrust(PluginBase):
                 )
             elif response.status_code != 200:
                 self.logger.error(
-                    f"ThirdPartyTrust Plugin: HTTP request returned with status code {response.status_code}."
+                    f"{PLUGIN_NAME}: HTTP request returned with "
+                    f"status code {response.status_code}."
                 )
                 return ValidationResult(
                     success=False,
@@ -255,7 +326,7 @@ class ThirdPartyTrust(PluginBase):
                 )
         except Exception as e:
             self.logger.error(
-                "ThirdPartyTrust Plugin: Error while fetching data from ThirdPartyTrust."
+                f"{PLUGIN_NAME}: Error while fetching data from BitSight."
                 + repr(e)
             )
             return ValidationResult(
@@ -265,7 +336,7 @@ class ThirdPartyTrust(PluginBase):
 
         return ValidationResult(
             success=True,
-            message="Validation successfull for ThirdPartyTrust plugin",
+            message="Validation successfull for BitSight plugin",
         )
 
     def handle_response(self, resp):
