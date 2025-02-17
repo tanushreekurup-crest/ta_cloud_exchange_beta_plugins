8d862499c814c3c604a46fb886f7630b8b5ed772
kamleshsolanki-crest <106314386+kamleshsolanki-crest@users.noreply.github.com>
2025-02-17 20:03:15 +0530

    Added CTE Mimecast 2.0.0 Plugin (#232)

--- mimecast/utils/mimecast_constants.py
+++ mimecast/utils/mimecast_constants.py
@@ -32,34 +32,28 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 CTE Mimecast plugin constants.
 """
 
-MAX_REQUEST_URL = 25
-MAX_CREATE_URL = 20
 PLUGIN_NAME = "Mimecast"
 MODULE_NAME = "CTE"
-PLUGIN_VERSION = "1.2.0"
-DEFAULT_WAIT_TIME = 60
+PLUGIN_VERSION = "2.0.0-beta"
+MAX_REQUEST_URL = 25
+MAX_CREATE_URL = 20
+DEFAULT_WAIT_TIME = 60000
 MAX_API_CALLS = 4
 PUSH_HASH_BATCH_SIZE = 1000
-PULL_URL_BATCH_SIZE = 500
-
+PULL_URL_BATCH_SIZE = 100
+RETRACTION_BATCH = 10000
+QUOTA_ERROR = "Quota not available to create the batch"
+# API Endpoints
+BASE_URL = "https://api.services.mimecast.com"
+GET_BEARER_TOKEN_ENDPOINT = "/oauth/token"
 GET_ACCOUNT_ENDPOINT = "/api/account/get-account"
 FETCH_HASHES_ENDPOINT = "/api/ttp/threat-intel/get-feed"
 FETCH_URL_ENDPOINT = "/api/ttp/url/get-logs"
 PUSH_HASH_ENDPOINT = "/api/byo-threat-intelligence/create-batch"
 DECODE_URL_ENDPOINT = "/api/ttp/url/decode-url"
 PUSH_URL_ENDPOINT = "/api/ttp/url/create-managed-url"
-
-
-
-
-GET_TYPE_FROM_MAPPING = {
-    "text": str,
-    "password": str,
-    "number": int,
-    "choice": str,
-    "multichoice": list,
-    "teaxtarea": str,
-}
+GET_URL_ENDPOINT = "/api/ttp/url/get-all-managed-urls"
+DELETE_URL_ENDPOINT = "/api/ttp/url/delete-managed-url"
 
 HASH_OPERATION_TYPE = {
     "ALLOW": "ALLOW",
@@ -72,18 +66,30 @@ URL_OPERATION_TYPE = {
     "PERMIT": "permit",
 }
 
+URL_MATCH_TYPE = {
+    "Explicit": "explicit",
+    "Domain": "domain",
+}
+
 MALWARE_TYPE = {
     "Malware Customer": "malware_customer",
-    "Malware Grid": "malware_grid"
+    "Malware Grid": "malware_grid",
 }
 
 FEED_TYPES = {
     "malware_customer": "Malware Customer",
     "malware_grid": "Malware Grid",
-    "malsite": "Malsite"
+    "malsite": "Malsite",
 }
 
-MALWARE_TYPES = [
-    "MD5",
-    "SHA256"
-]
+MALWARE_TYPES = ["MD5", "SHA256"]
+
+# Maximum number of Initial Range in Days.
+INTEGER_THRESHOLD = 4611686018427387904
+
+# Tag Name
+PREFIX_IOC_SOURCE = "Netskope CE"
+SEPARATOR = "|"
+
+# Retraction Constant
+RETRACTION = "Retraction"
